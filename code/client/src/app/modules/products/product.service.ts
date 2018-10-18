88e7350513005cb193f291b9e09e9a8a47ac2f40
Samuel Nettey <snettey@hotmail.de>
2018-10-18 08:29:02 +0200

    removed white spaces from products service page

--- code/client/src/app/modules/products/product.service.ts
+++ code/client/src/app/modules/products/product.service.ts
@@ -2,30 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, tap, map } from 'rxjs/operators';
-
 import { IProduct } from './product';
-
 @Injectable({
-   providedIn: 'root'
+  providedIn: 'root'
 })
 export class ProductService {
   private productUrl = 'api/products/products.json';
-
-  constructor(private http: HttpClient) { }
-
+  constructor(private http: HttpClient) {}
   getProducts(): Observable<IProduct[]> {
     return this.http.get<IProduct[]>(this.productUrl).pipe(
       tap(data => console.log('All: ' + JSON.stringify(data))),
       catchError(this.handleError)
     );
   }
-
   getProduct(id: number): Observable<IProduct | undefined> {
     return this.getProducts().pipe(
       map((products: IProduct[]) => products.find(p => p.productId === id))
     );
   }
-
   private handleError(err: HttpErrorResponse) {
     // in a real world app, we may send the server to some remote logging infrastructure
     // instead of just logging it to the console
@@ -36,7 +30,9 @@ export class ProductService {
     } else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong,
-      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
+      errorMessage = `Server returned code: ${err.status}, error message is: ${
+        err.message
+      }`;
     }
     console.error(errorMessage);
     return throwError(errorMessage);
